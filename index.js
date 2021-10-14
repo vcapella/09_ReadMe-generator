f1ded532d43f1fa77f70f35897d2f643af8369f1
Victor Capella <capella89@outlook.com>
2021-10-14 01:14:26 -0400

    small update on JS files.

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
 const questions = [
   {
     type: "input",
@@ -59,12 +57,10 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((answers) => {
     console.log(answers);
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,7 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   return `![License](https://img.shields.io/badge/License-${license}-blue.svg)`;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README (for each section)
 function generateMarkdown(data) {
   return `# ${data.title}
 
